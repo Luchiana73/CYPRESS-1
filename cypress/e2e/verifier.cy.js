5a64eeb59ce6ac1c2d6bcc8cfdb723c04cdd443d
Svetlana <luchiana.sv@gmail.com>
2023-12-04 14:21:18 +0100

    added new spec file with one test script - hw 5.1.2

---
+++ cypress/e2e/verifier.cy.js
@@ -0,0 +1,6 @@
+describe("visit site and check the page is loaded", () => {
+  it("should visit site and check page loaded", () => {
+    cy.visit("https://sqlverifier-live-6e21ca0ed768.herokuapp.com");
+    cy.get(".brand-title").should("exist").and("be.visible");
+  });
+});
